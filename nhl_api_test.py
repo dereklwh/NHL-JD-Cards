03f96b910b6b126a6d0ed9bb427bab9455a0a5b5
Derek <derek141772@gmail.com>
2024-11-22 21:56:02 -0800

    testing nhl api connection

---
+++ nhl_api_test.py
@@ -0,0 +1,21 @@
+# imports
+import requests
+import json
+import numpy as np
+
+print('Hi!')
+
+base_url = 'https://api-web.nhle.com/v1/'
+end_point = 'roster/VAN/current'
+# API TEST
+url = base_url + end_point
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    # Process the data as needed
+else:
+    print(f'Error: {response.status_code}')
+
+print(data)
+
