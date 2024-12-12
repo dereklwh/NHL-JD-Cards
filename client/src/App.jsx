2f94046cca0a9b6723c5324badcbc9cf61460083
dereklwh <derek141772@gmail.com>
2024-12-11 18:32:42 -0800

    Implement player search suggestions and update animations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,8 @@ function App() {
 
   const [player, setPlayer] = useState(null);
   const [searchInput, setSearchInput] = useState("");
-  const [playerId, setPlayerId] = useState('8478402')
+  const [playerId, setPlayerId] = useState('8478402');
+  const [suggestions, setSuggestions] = useState([]);
 
   const fetchPlayer = async (id) => {
     try {
@@ -19,24 +20,46 @@ function App() {
     }
   };
 
-  useEffect(() => {
-    fetchPlayer(playerId);
-  }, [playerId]);
-
+  // handles searching for playerid
   const handleSearch = () => {
     setPlayerId(searchInput);
   }
 
+  // if the user presses enter, search for the player
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
       handleSearch();
     }
   };
 
+  // fetches list of players that match the search query
+  const fetchSuggestions = async (query) => {
+    if (!query) {
+      setSuggestions([]);
+      return;
+    }
+    try {
+      const response = await fetch(`http://localhost:3000/search?name=${query}`);
+      const data = await response.json();
+      setSuggestions(data);
+    } catch (error) {
+      console.error('Error fetching suggestions:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPlayer(playerId);
+  }, [playerId]);
+
+  useEffect(() => {
+    fetchSuggestions(searchInput);
+  }, [searchInput]);
+
 
   return (
     <div className="app-container">
       <div>
+        <h1 style={{ textAlign: "center" }}>Player Cards Display</h1>
         <div style={{ textAlign: "center", marginBottom: "20px" }}>
           <input
             type="text"
@@ -46,6 +69,17 @@ function App() {
             placeholder="Enter player ID"
             style={{padding: '8px', width: '200px', marginRight: '10px'}}
           />
+          {suggestions.length > 0 && (<ul style={{ listStyleType: 'none', background: '#fff', margin: '0', padding: '0', zIndex: 1000, position: 'absolute'}}>
+            {suggestions.map((player) => (
+              <li 
+                key={player['Player ID']} onClick={() => setPlayerId(player['Player ID'])}
+                style={{color: '#333'}}
+                className="suggestion-item"
+              >
+                {player['First Name']} {player['Last Name']}
+              </li>
+            ))}s
+          </ul>)}
           <button onClick={handleSearch}>Search</button>
         </div>
         <TransitionGroup>
@@ -55,7 +89,6 @@ function App() {
             classNames="fade"
           >
             <div>
-              <h1 style={{ textAlign: "center" }}>Player Card</h1>
               {player ? (
                 <PlayerCard player={player} />
               ) : (
